6ad2a5113ea26d26ebe93b1c2f0113d051c74830
Matheus Sanders Montandon <msmontandon@gmail.com>
2022-03-21 19:50:17 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+var width = 7;
+var height = 4;
+var area = width * height;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof (area));
+
+var bill = 24.5;
+var payment = 30;
+var change = payment - bill;
+
+console.log('value of change:', change);
+console.log('typeof change:', typeof (change));
+
+var quizzes = 95;
+var midterm = 90;
+var final = 85;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof (grade));
+
+var firstName = 'Ron';
+var lastName = 'Swanson';
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof (fullName));
+
+var pH = 4.8;
+var isAcid = pH < 7;
+
+console.log('value of isAcid:', isAcid);
+console.log('typeof isAcid:', typeof (isAcid));
+
+var headCount = 200;
+var isSparta = headCount === 300;
+
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof (isSparta));
+
+var motto = fullName;
+motto += ' is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof (motto));

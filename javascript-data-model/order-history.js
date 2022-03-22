76102bbdc78d01ab5096f270e596fe85970c26a5
Matheus Sanders Montandon <msmontandon@gmail.com>
2022-03-22 18:14:04 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,95 @@
+var orderHistory = [
+  {
+    orderPlacementDate: [8, 4, 2020],
+    orderDeliveryDate: [8, 8, 2020],
+    totalPrice: ['$', 34.00],
+    shippedTo: 'JS Masher',
+    orderNumber: [114, 3941689, 8772232],
+    handedDirectly: false,
+    // numberOfItems: 1,
+    item: [
+      {
+        img: 'placeholder.url',
+        name: 'JavaScript for impatient programmers',
+        hasAuthor: true,
+        authors: ['Rauschmayer', 'Dr. Axel'],
+        reuturnWindownClosed: true,
+        returnWindown: [9, 7, 2020],
+        price: ['$', 31.55]
+      }
+    ],
+    hasReview: false
+  },
+  {
+    orderPlacementDate: [7, 19, 2020],
+    orderDeliveryDate: [7, 20, 2020],
+    totalPrice: ['$', 44.53],
+    shippedTo: 'JS Masher',
+    orderNumber: [113, 9984268, 1280257],
+    handedDirectly: true,
+    // numberOfItems: 1,
+    item: [
+      {
+        img: 'placeholder.url',
+        name: 'The Timeless Way of Building',
+        hasAuthor: true,
+        authors: ['Alexander', 'Christopher'],
+        reuturnWindownClosed: true,
+        returnWindown: [8, 19, 2020],
+        price: ['$', 41.33]
+      }
+    ],
+    hasReview: false
+  },
+  {
+    orderPlacementDate: [7, 4, 2020],
+    orderDeliveryDate: [7, 7, 2020],
+    totalPrice: ['$', 17.22],
+    shippedTo: 'JS Masher',
+    orderNumber: [114, 2875557, 9059409],
+    handedDirectly: true,
+    // numberOfItems: 1,
+    item: [
+      {
+        img: 'placeholder.url',
+        name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Apater for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        hasAuthor: false,
+        authors: null,
+        reuturnWindownClosed: true,
+        returnWindown: [8, 5, 2020],
+        price: ['$', 15.98]
+      }
+    ],
+    hasReview: false
+  },
+  {
+    orderPlacementDate: [7, 3, 2020],
+    orderDeliveryDate: [7, 5, 2020],
+    totalPrice: ['$', 138.98],
+    shippedTo: 'JS Masher',
+    orderNumber: [113, 2883177, 2648248],
+    handedDirectly: false,
+    // numberOfItems: 2,
+    item: [
+      {
+        img: 'placeholder.url',
+        name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        hasAuthor: false,
+        authors: null,
+        reuturnWindownClosed: true,
+        returnWindown: [8, 4, 2020],
+        price: ['$', 94.95]
+      },
+      {
+        img: 'placeholder.url',
+        name: 'The Art of Sql',
+        hasAuthor: true,
+        authors: ['Faroult', 'Stephane'],
+        reuturnWindownClosed: true,
+        returnWindown: [8, 4, 2020],
+        price: ['$', 33.99]
+      }
+    ],
+    hasReview: false
+  }
+];

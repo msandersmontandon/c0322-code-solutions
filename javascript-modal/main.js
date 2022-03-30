c85815f756f98e5c38511303375e0b1f7c8a6f91
Matheus Sanders Montandon <msmontandon@gmail.com>
2022-03-30 04:46:44 +0000

    Revised javascript-modal challenge... now with display:none;

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,15 +1,17 @@
-var $body = document.querySelector('body');
+// var $body = document.querySelector('body');
 var $modalBackground = document.querySelector('.modal-background');
 var $modalNo = document.querySelector('.no');
-$body.removeChild($modalBackground);
+// $body.removeChild($modalBackground);
 var $openModal = document.querySelector('.open-modal');
 function modalOn(event) {
-  $body.appendChild($modalBackground);
-  $modalNo = document.querySelector('.no');
+  // $body.appendChild($modalBackground);
+  // $modalNo = document.querySelector('.no');
+  $modalBackground.className = 'modal-background';
   $modalNo.addEventListener('click', modalOff);
 }
 function modalOff(event) {
-  $body.removeChild($modalBackground);
+  // $body.removeChild($modalBackground);
+  $modalBackground.className = 'moda-backgorund hide';
   $openModal.addEventListener('click', modalOn);
 }
 $openModal.addEventListener('click', modalOn);

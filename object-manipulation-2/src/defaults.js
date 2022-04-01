e1b1eb8f973361c8f09eb0ea3a6ac1ee5a2ad506
Matheus Sanders Montandon <msmontandon@gmail.com>
2022-04-01 17:03:38 +0000

    In progress...

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,8 @@
 /* exported defaults */
+
+/** pseudocode for default
+ * 1. make the function definition for default() with 2 paramethers: target and source;
+ * 2. use a for in loop to iterate through the sourceKey property names of the source paramether;
+ * 3. use another fo in loop to iterate through the targetKey property names of the property paramether;
+ *
+ */
--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,17 @@
 /* exported invert */
+
+/** pseudocode for invert
+ * 1. make the function definition for invert() wit 1 paramether: source;
+ * 2. declare a variable invertedObject and assign an empty object literal to it;
+ * 3. use a for in loop to iterate through the key property names of the source paramether;
+ * 4. assign the inverted pair value:property of the current key of the source paramether to the invertedObject object;
+ * 5. after the loop, return from the function the value of the invertedObject variable.
+ */
+
+function invert(source) {
+  var invertedObject = {};
+  for (var key in source) {
+    invertedObject[source[key]] = key;
+  }
+  return invertedObject;
+}

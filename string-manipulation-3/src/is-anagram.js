3e782b5e3be54b6992867019df88dfb5b290d0e0
Matheus Sanders Montandon <msmontandon@gmail.com>
2022-04-08 01:17:56 +0000

    Completed string-manipulation-3 algorithms.

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -8,9 +8,37 @@
  * we either gonna have two arrays of boolean trues, or a mismatche will show itself as false value within;
  * now, to the pseudocode:
  * 1. make the function definition isAnagram() with two paramethers: firstString and secondString;
- * 2. declare a variable firstArray, and assign to it the array of strings split from the firstString paramether by the empty spaces;
+ * 2. declare a variable firstArray and assign to it the array of words split from the firstString paramether by the empty spaces;
  * 3. declare a variable temporaryString and assign an empty string to it;
- * 4. Using a for loop
+ * 4. Using a for loop, iterate through the indexes of the firstArray array;
+ * 5. concatenate the value of the temporaryString string with the value of the string at the current index of the firstArray array, and then:
+ * 6. assign the result of that expression back to the temporaryString variable;
+ * 7. take the value of the temporaryString string to lower case and assign it back to the temporaryString variable;
+ * 8. assign to the firstArray variable the array of characters split from the temporaryString string;
+ * 9. declare a variable secondArray and assign to it the array of words split from the secondString paramether by the empty spaces;
+ * 10. assign an empty string to the temporaryString variable;
+ * 11. Using a for loop, iterate through the indexes of the secondArray array;
+ * 12. concatenate the value of the temporaryString string with the value of the string at the current index of the secondArray array, and then:
+ * 13. assign the result of that expression back to the temporaryString variable;
+ * 14. take the value of the temporaryString string to lower case and assign it back to the temporaryString variable;
+ * 15. assign to the secondArray variable the array of characters split from the temporaryString string;
+ * 16. use an if statement to guard against the case in which the lengths of the firstArray array and of the secondArray array don't match;
+ * 17. if they don't, return the boolean false from the function;
+ * 18. otherwise, declare a variable firstMatch and assign an empty array literal to it;
+ * 19. use a for loop to iterate through the firstIndex indexes of the firstArray array;
+ * 20. append the boolean value of false to the end of the firstMatch array;
+ * 21. after the end of that for loop, declare a variable secondMatch and assign an empty array literal to it;
+ * 19. use a for loop to iterate through the secondIndex indexes of the secondArray array;
+ * 20. append the boolean value of false to the end of the secondMatch array;
+ * 21. use a for loop to iterate through the firstIndex indexes of the firstArray array;
+ * 22. use another for loop to iterate through the secondIndex indexes of the secondArray array;
+ * 23. if neither the value at the current firstIndex of the firstMatch array nor the value at the current secondIndex of the secondMatch array are true, and:
+ * 24. the character at the firstIndex of the firstArray array matches with the character at the current secondIndex of the secondArray array, then:
+ * 25. assign the boolean value of true to the current firstIndex of the firstMatch array, and:
+ * 26. assign the boolean value of true to the current secondIndex of the secondMatch array;
+ * 27. then, after all if statement and for loops, return from the function:
+ * 28. the boolean value of false, if there still is any boolean values of false inside the firstMatch array:
+ * 29. or the boolean value of true otherwise;
  */
 
 function isAnagram(firstString, secondString) {
@@ -21,7 +49,6 @@ function isAnagram(firstString, secondString) {
   }
   temporaryString = temporaryString.toLowerCase();
   firstArray = temporaryString.split('');
-  // console.log(firstArray);
 
   var secondArray = secondString.split(' ');
   temporaryString = '';
@@ -30,7 +57,6 @@ function isAnagram(firstString, secondString) {
   }
   temporaryString = temporaryString.toLowerCase();
   secondArray = temporaryString.split('');
-  // console.log(secondArray);
 
   if (firstArray.length !== secondArray.length) {
     return false;
@@ -53,7 +79,5 @@ function isAnagram(firstString, secondString) {
       }
     }
   }
-  // console.log(firstMatch);
-  // console.log(secondMatch);
   return !firstMatch.includes(false);
 }
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -6,10 +6,109 @@
  * b. Capitalize all “major” words (nouns, verbs, adjectives, adverbs, and pronouns) in the title/heading, including the second part of hyphenated major words (e.g., Self-Report not Self-report); and
  * c. Capitalize all words of four letters or more.
  * which leads to a fourth direction:
- * d. Only "minor" words of three letters or fewer should be lowercase, which means that only only conjunctions and prepositions that don't fall under a. will be lowrcased.
- * now, I planned to have roughly 1 function for each of these 4 rules, but it turns out I'll be only exporting the titleCase() function to be tested;
- * nevertheless, I decided to leave commented out the pieces of thoses function in the code, as they give clues to my decision-making process for this algorithm;
+ * d. Only "minor" words of three letters or fewer should be lowercase, which means that only only conjunctions and prepositions that don't fall under a. will be lowercased.
+ * with these directives in mind, here's the code to implement:
  * 1. make the function definition for titleCase() with 1 paramether: title;
+ * 2. declare the variable lowerCased and assign to it the string value of th title paramether taken to lowercase;
+ * 3. declare the variable titleWords and assign to it the array of strings split from the lowersCased string at the empty spaces;
+ * 4. declare the variable colon and assign the boolean value of false to it;
+ * 5. declare the variable dealtWith and assign the boolean value of false to it;
+ * 6. declare the variable capitalLetter and assign an empty string to it;
+ * 7. declare the variable titleCased and assign an empty string to it;
+ * 8. declare the variabe currentWord and assign an empty string to it;
+ * 9. using a for loop, iterate through the indexes of the titleWords array;
+ * 10. assign the string at the current index of the titleWords array to the currentWord variable;
+ * 11. reassign the boolean value of false to the dealtWith variable;
+ * 12. using an if statement, check if the currentWord string includes the dash character;
+ * 13. if it does, declare a new variable wordsArray and assign to it the array of strings split from the currentWord string at the dashes;
+ * 14. declare the variable local word and assign an empty string to it;
+ * 15. assign an empty string to the currentWord variable;
+ * 16. using a for loop, iterate through the indexes of the wordsArray array;
+ * 17. reassign the boolean value of false to the dealtWith variable;
+ * 18. assign the string at the current index of the wordsArray array to the localWord array;
+ * 19. usign a switch statement, check the value of the localWord string against the following cases:
+ * 20. in the case of matching with the string 'javascript':
+ * 21. assign the string 'JavaScript' to the localWord variable;
+ * 22. assign the boolean value of false to the colon variable;
+ * 23. assign the boolean value of true to the dealtWith variable;
+ * 24. break from the switch cases;
+ * 25. then in the case of matching with the string 'javascript:':
+ * 26. assign the string 'JavaScript:' to the localWord variable;
+ * 27. assign the boolean value of true to the colon variable;
+ * 28. assign the boolean value of true to the dealtWith variable;
+ * 29. break from the switch cases;
+ * 30. then in the case of matching with the string 'api':
+ * 31. assign the string 'API' to the localWord variable;
+ * 32. assign the boolean value of false to the colon variable;
+ * 33. assign the boolean value of true to the dealtWith variable;
+ * 34. break from the switch cases;
+ * 35. then in the case of matching with the string 'api:':
+ * 36. assign the string 'API:' to the localWord variable;
+ * 37. assign the boolean value of true to the colon variable;
+ * 38. assign the boolean value of true to the dealtWith variable;
+ * 39. after the end of the switch cases, use an if statement to check if the dealtWith variable is still false;
+ * 40. if it still is, reassign the boolean value of false to the colon variable;
+ * 41. assign the character at the first index of the localWord string to the capitalLetter variable;
+ * 42. assign the rest of the localWord string to the lowerCased variable;
+ * 43. concatenate the capitalLetter string taken to the upper letter with the lowerCased string, and then:
+ * 44. assign the result of that expression to the localWord variable;
+ * 45. then use an if statement to check if the localWord string ends with a colon;
+ * 46. if it does, assign the boolean value of true to the colon variable;
+ * 47. then after the end of that if statement, assign the boolean value of true to the dealtWith variable;
+ * 48. then after the end of that if statement, concatenate the currentWord string with the localWord string, and then:
+ * 49. concatenate the result of that expression to a dash, and then assign the result of that expression back to the currentWord variable;
+ * 50. then after the end of the for loop, assign the string sliced from the beggining until the second to last character of the currentWord string back to the currentWord variable;
+ * 51. then after the end of that if statement, use an if statement to check if the dealtWith variable is still false;
+ * 52. if it still is, use a switch statement to check the value of the currentWord string against the following cases:
+ * 53. in the case of matching with the string 'javascript':
+ * 54. assign the string 'JavaScript' to the localWord variable;
+ * 55. assign the boolean value of false to the colon variable;
+ * 56. assign the boolean value of true to the dealtWith variable;
+ * 57. break from the switch cases;
+ * 58. then in the case of matching with the string 'javascript:':
+ * 59. assign the string 'JavaScript:' to the localWord variable;
+ * 60. assign the boolean value of true to the colon variable;
+ * 61. assign the boolean value of true to the dealtWith variable;
+ * 62. break from the switch cases;
+ * 63. then in the case of matching with the string 'api':
+ * 64. assign the string 'API' to the localWord variable;
+ * 65. assign the boolean value of false to the colon variable;
+ * 66. assign the boolean value of true to the dealtWith variable;
+ * 67. break from the switch cases;
+ * 68. then in the case of matching with the string 'api:':
+ * 69. assign the string 'API:' to the localWord variable;
+ * 70. assign the boolean value of true to the colon variable;
+ * 71. assign the boolean value of true to the dealtWith variable;
+ * 72. then after the end of the switch cases, and of that if statement, use an if statement to check for loop index for the titleWords array is past the first;
+ * 73. if it already is, use an if statement to check if the colon variable is true;
+ * 74. if it is, reassign the boolean value of false to the colon variable;
+ * 75. assign the character at the first index of the currentWord string to the capitalLetter variable;
+ * 76. assign the rest of the currentWord string to the lowerCased variable;
+ * 77. concatenate the capitalLetter string taken to the upper letter with the lowerCased string, and then:
+ * 78. assign the result of that expression to the currentWord variable;
+ * 79. then use an if statement to check if the currentWord string ends with a colon;
+ * 80. if it does, assign the boolean value of true to the colon variable;
+ * 81. then after the end of that if statement, assign the boolean value of true to the dealtWith variable;
+ * 82. then after the end of that if statement, use an if statement to check if the dealtWith variable is still false;
+ * 83. if it still is, use a switch statement to check the value of the currentWord string against the following cases:
+ * 84. in the case of matching with any between the strings 'in', 'the', 'of', 'for', 'to', 'on' or 'and':
+ * 85. assign the boolean value of false to the colon variable;
+ * 86. assign the boolean value of true to the dealtWith variable;
+ * 87. then in the case of matching with any between the strings 'in:', 'the:', 'of:', 'for:', 'to:', 'on:' or 'and:':
+ * 88. assign the boolean value of true to the colon variable;
+ * 89. assign the boolean value of true to the dealtWith variable;
+ * 90. then after the end of the switch cases, and the past two if statements, use an if statement to check if the dealtWith variable is still false;
+ * 91. if it still is, assign the character at the first index of the currentWord string to the capitalLetter variable;
+ * 92. assign the rest of the currentWord string to the lowerCased variable;
+ * 93. concatenate the capitalLetter string taken to the upper letter with the lowerCased string, and then:
+ * 94. assign the result of that expression to the currentWord variable;
+ * 95. then use an if statement to check if the currentWord string ends with a colon;
+ * 96. if it does, assign the boolean value of true to the colon variable;
+ * 97. then after the end of that if statement, assign the boolean value of true to the dealtWith variable;
+ * 98. then after the end of that if statement, concatenate the titleCased string with the currentWord string, and then:
+ * 99. concatonate the result of that expression with an empty space, and then assign the result of that expression back to the titleCased variable;
+ * 100. then after the for loop, trim the end of the titleCased string and assign it back to the titleCased variable;
+ * 101. return from the function the value of the titleCased string;
  */
 
 function titleCase(title) {
@@ -21,134 +120,9 @@ function titleCase(title) {
   var titleCased = '';
   var currentWord = '';
 
-  // function caseWord(word) {
-  //   if (dealtWith) {
-  //     return;
-  //   }
-  //   colon = false;
-  //   capitalLetter = word.at(0);
-  //   lowerCased = word.slice(1);
-  //   word = capitalLetter.toUpperCase() + lowerCased;
-  //   if (word.endsWith(':')) {
-  //     colon = true;
-  //   }
-  //   dealtWith = true;
-  // }
-
-  // function specialCase(word) {
-  //   if (dealtWith) {
-  //     return;
-  //   }
-  //   if (word === 'javascript') {
-  //     word = 'JavaScript';
-  //     colon = false;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'javascript:') {
-  //     word = 'JavaScript:';
-  //     colon = true;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'api') {
-  //     word = 'API';
-  //     colon = false;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'api:') {
-  //     word = 'API:';
-  //     colon = true;
-  //     dealtWith = true;
-  //   }
-  // }
-
-  // function dashCase(word) {
-  //   if (word.includes('-')) {
-  //     var wordsArray = word.split('-');
-  //     var localWord = '';
-  //     word = '';
-  //     for (var j = 0; j < wordsArray.length; j++) {
-  //       dealtWith = false;
-  //       localWord = wordsArray[j];
-  //       specialCase(localWord);
-  //       caseWord(localWord);
-  //       word += localWord + '-';
-  //     }
-  //     word = word.slice(0, word.length - 1);
-  //   }
-  // }
-
-  // function exception(word) {
-  //   if (dealtWith) {
-  //     return;
-  //   }
-  //   if (word === 'in') {
-  //     colon = false;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'in:') {
-  //     colon = true;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'the') {
-  //     colon = false;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'the:') {
-  //     colon = true;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'of') {
-  //     colon = false;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'of:') {
-  //     colon = true;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'for') {
-  //     colon = false;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'for:') {
-  //     colon = true;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'to') {
-  //     colon = false;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'to:') {
-  //     colon = true;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'on') {
-  //     colon = false;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'on:') {
-  //     colon = true;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'and') {
-  //     colon = false;
-  //     dealtWith = true;
-  //   }
-  //   if (word === 'and:') {
-  //     colon = true;
-  //     dealtWith = true;
-  //   }
-  // }
-
-  // currentWord = titleWords[0];
-  // dashCase(currentWord);
-  // specialCase(currentWord);
-  // caseWord(currentWord);
-  // titleCased += currentWord + ' ';
   for (var i = 0; i < titleWords.length; i++) {
     currentWord = titleWords[i];
     dealtWith = false;
-    // dashCase(currentWord);
-    // function dashCase(word) {
     if (currentWord.includes('-')) {
       var wordsArray = currentWord.split('-');
       var localWord = '';
@@ -156,30 +130,27 @@ function titleCase(title) {
       for (var j = 0; j < wordsArray.length; j++) {
         dealtWith = false;
         localWord = wordsArray[j];
-        // specialCase(localWord);
-        if (!dealtWith) {
-          if (localWord === 'javascript') {
+        switch (localWord) {
+          case 'javascript':
             localWord = 'JavaScript';
             colon = false;
             dealtWith = true;
-          }
-          if (localWord === 'javascript:') {
+            break;
+          case 'javascript:':
             localWord = 'JavaScript:';
             colon = true;
             dealtWith = true;
-          }
-          if (localWord === 'api') {
+            break;
+          case 'api':
             localWord = 'API';
             colon = false;
             dealtWith = true;
-          }
-          if (localWord === 'api:') {
+            break;
+          case 'api:':
             localWord = 'API:';
             colon = true;
             dealtWith = true;
-          }
         }
-        // caseWord(localWord);
         if (!dealtWith) {
           colon = false;
           capitalLetter = localWord.at(0);
@@ -194,110 +165,65 @@ function titleCase(title) {
       }
       currentWord = currentWord.slice(0, currentWord.length - 1);
     }
-    // }
-    // specialCase(currentWord);
-    // function specialCase(word) {
     if (!dealtWith) {
-      if (currentWord === 'javascript') {
-        currentWord = 'JavaScript';
-        colon = false;
-        dealtWith = true;
-      }
-      if (currentWord === 'javascript:') {
-        currentWord = 'JavaScript:';
-        colon = true;
-        dealtWith = true;
-      }
-      if (currentWord === 'api') {
-        currentWord = 'API';
-        colon = false;
-        dealtWith = true;
-      }
-      if (currentWord === 'api:') {
-        currentWord = 'API:';
-        colon = true;
-        dealtWith = true;
+      switch (currentWord) {
+        case 'javascript':
+          currentWord = 'JavaScript';
+          colon = false;
+          dealtWith = true;
+          break;
+        case 'javascript:':
+          currentWord = 'JavaScript:';
+          colon = true;
+          dealtWith = true;
+          break;
+        case 'api':
+          currentWord = 'API';
+          colon = false;
+          dealtWith = true;
+          break;
+        case 'api:':
+          currentWord = 'API:';
+          colon = true;
+          dealtWith = true;
       }
     }
     if (i > 0) {
       if (colon) {
-        // caseWord(currentWord);
-        // function caseWord(word) {
-        if (!dealtWith) {
-          colon = false;
-          capitalLetter = currentWord.at(0);
-          lowerCased = currentWord.slice(1);
-          currentWord = capitalLetter.toUpperCase() + lowerCased;
-          if (currentWord.endsWith(':')) {
-            colon = true;
-          }
-          dealtWith = true;
+        colon = false;
+        capitalLetter = currentWord.at(0);
+        lowerCased = currentWord.slice(1);
+        currentWord = capitalLetter.toUpperCase() + lowerCased;
+        if (currentWord.endsWith(':')) {
+          colon = true;
         }
+        dealtWith = true;
       }
-      // exception(currentWord);
-      // function exception(word) {
       if (!dealtWith) {
-        if (currentWord === 'in') {
-          colon = false;
-          dealtWith = true;
-        }
-        if (currentWord === 'in:') {
-          colon = true;
-          dealtWith = true;
-        }
-        if (currentWord === 'the') {
-          colon = false;
-          dealtWith = true;
-        }
-        if (currentWord === 'the:') {
-          colon = true;
-          dealtWith = true;
-        }
-        if (currentWord === 'of') {
-          colon = false;
-          dealtWith = true;
-        }
-        if (currentWord === 'of:') {
-          colon = true;
-          dealtWith = true;
-        }
-        if (currentWord === 'for') {
-          colon = false;
-          dealtWith = true;
-        }
-        if (currentWord === 'for:') {
-          colon = true;
-          dealtWith = true;
-        }
-        if (currentWord === 'to') {
-          colon = false;
-          dealtWith = true;
-        }
-        if (currentWord === 'to:') {
-          colon = true;
-          dealtWith = true;
-        }
-        if (currentWord === 'on') {
-          colon = false;
-          dealtWith = true;
-        }
-        if (currentWord === 'on:') {
-          colon = true;
-          dealtWith = true;
-        }
-        if (currentWord === 'and') {
-          colon = false;
-          dealtWith = true;
-        }
-        if (currentWord === 'and:') {
-          colon = true;
-          dealtWith = true;
+        switch (currentWord) {
+          case 'in':
+          case 'the':
+          case 'of':
+          case 'for':
+          case 'to':
+          case 'on':
+          case 'and':
+            colon = false;
+            dealtWith = true;
+            break;
+          case 'in:':
+          case 'the:':
+          case 'of:':
+          case 'for:':
+          case 'to:':
+          case 'on:':
+          case 'and:':
+            colon = false;
+            dealtWith = true;
         }
       }
     }
-    // caseWord(currentWord);
     if (!dealtWith) {
-      colon = false;
       capitalLetter = currentWord.at(0);
       lowerCased = currentWord.slice(1);
       currentWord = capitalLetter.toUpperCase() + lowerCased;
@@ -306,7 +232,6 @@ function titleCase(title) {
       }
       dealtWith = true;
     }
-    // console.log(currentWord);
     titleCased += currentWord + ' ';
   }
   titleCased = titleCased.trimEnd();

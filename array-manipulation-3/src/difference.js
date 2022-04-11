35ecf193e35542c743320a3209e4406b6cf520bf
Matheus Sanders Montandon <msmontandon@gmail.com>
2022-04-11 17:16:00 +0000

    In progress...

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,7 @@
 /* exported difference */
+
+/** pseudocode for difference
+ * 1. make the function definition for difference() with 2 paramethers: first and second;
+ * 2. declare the variable differenceArray and assign an empty array literal to it;
+ * 3.
+ */

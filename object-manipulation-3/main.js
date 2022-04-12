faeebd3202996c4f1e20e6fb6dbfe78670607191
Matheus Sanders Montandon <msmontandon@gmail.com>
2022-04-12 17:20:48 +0000

    In progress...

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,15 +1,24 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
-
+/* the folowing object will represent the game as a whole */
 var game = {
-  players: [],
-  deck: [],
-  handSize: 1,
-  setPlayers: function (playersList) {
+
+  /* the properties wil include palyers, a deck, an the handsize for the game */
+  players: [], // an array of palyers objects, each with a name, a hand of cards, and a total of points for the game;
+  deck: [], // the deck of cards, shuffled at the beginning and after each round, from where the cards will be dealt;
+  handSize: 1, // the hand size for the game, set to one as the default value;
+
+  /** the methods for the game, including:
+   * set methods to set the players, the deck and the game as a whole;
+   * methods to deal and to discard a hand of cards to a giving player, as well as reading the value of a giving palyers hand;
+   * methods to start a new round, as well for the end round, seing who won and if there's a need for a tiebreaker game;
+   */
+  setPlayers: function (playersList) { //
     var playersArray = [];
     for (var p = 0; p < playersList.length; p++) {
       playersArray.push({
         name: playersList[p],
-        hand: []
+        hand: [],
+        points: 0
       });
     }
     this.players = playersArray;
@@ -52,7 +61,7 @@ var game = {
       }
       totalValue += value;
     }
-    currentPlayer.points = totalValue;
+    currentPlayer.points += totalValue;
   },
   deal: function (currentPlayer) {
     for (var h = 0; h < this.handSize; h++) {
@@ -71,13 +80,13 @@ var game = {
     this.deck = _.shuffle(this.deck);
     for (p = 0; p < this.players.length; p++) {
       this.deal(this.players[p]);
+      this.handValue(this.players[p]);
     }
   },
   endRound: function () {
     var highestScore = 0;
     var champions = [];
     for (var p = 0; p < this.players.length; p++) {
-      this.handValue(this.players[p]);
       if (this.players[p].points > highestScore) {
         highestScore = this.players[p].points;
       }
@@ -86,7 +95,7 @@ var game = {
       if (this.players[p].points === highestScore) {
         champions.push(this.players[p].name);
       }
-      delete this.players.poins;
+      this.players.poins = 0;
     }
     if (champions.length === 1) {
       console.log('The winner is', champions[0] + '!');

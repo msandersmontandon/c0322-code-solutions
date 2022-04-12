ee0a05977f7b6ad80e58d90207e02f29adf3d666
Matheus Sanders Montandon <msmontandon@gmail.com>
2022-04-12 22:22:47 +0000

    Completed object-manipulation 3 algorithm.

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -12,9 +12,9 @@ var game = {
    * methods to deal and to discard a hand of cards to a giving player, as well as reading the value of a giving palyers hand;
    * methods to start a new round, as well for the end round, seing who won and if there's a need for a tiebreaker game;
    */
-  setPlayers: function (playersList) { //
-    var playersArray = [];
-    for (var p = 0; p < playersList.length; p++) {
+  setPlayers: function (playersList) { // a method that take an array of players name (strings) and create for each one an entry at the playersArray array
+    var playersArray = []; // each entry is an onject with the name of the player, and empty hand and no points
+    for (var p = 0; p < playersList.length; p++) { // in the end, it sets the playersArray as the players property of the current game
       playersArray.push({
         name: playersList[p],
         hand: [],
@@ -23,8 +23,8 @@ var game = {
     }
     this.players = playersArray;
   },
-  setDeck: function () {
-    var newDeck = [];
+  setDeck: function () { // a method that populate a newDeck array with card objects, each with a rank and a suit.
+    var newDeck = []; // then it shuffles the newDeck and sets it as the deck property for the current game
     var suits = ['clubs', 'diamonds', 'hearts', 'spades'];
     var ranks = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
     for (var s = 0; s < suits.length; s++) {
@@ -37,14 +37,14 @@ var game = {
     }
     this.deck = _.shuffle(newDeck);
   },
-  setGame: function (listUpdate, sizeUpdate = 1) {
-    this.setPlayers(listUpdate);
-    this.setDeck();
+  setGame: function (listUpdate, sizeUpdate = 1) { // a method that will call the setPlayers and setDeck methods to set the players for the game,
+    this.setPlayers(listUpdate); // based on the listUpdate argument; will set a brand new, shuffled deck; will set the hand size for the game based
+    this.setDeck(); // on the sizeUpdate argument, and will even declare the players on the console.
     this.handSize = sizeUpdate;
     console.log('Game set between', _.join(listUpdate.splice(0, listUpdate.length - 1), ', ') + ' and', listUpdate.at(listUpdate.length - 1) + '!');
   },
-  handValue: function (currentPlayer) {
-    var totalValue = 0;
+  handValue: function (currentPlayer) { // a method that will update the points of the current player by adding the points of each individual card in
+    var totalValue = 0; // their hand
     var value = 0;
     for (var h = 0; h < currentPlayer.hand.length; h++) {
       switch (currentPlayer.hand[h].rank) {
@@ -63,18 +63,18 @@ var game = {
     }
     currentPlayer.points += totalValue;
   },
-  deal: function (currentPlayer) {
+  deal: function (currentPlayer) { // a method that will deal a hand to a player
     for (var h = 0; h < this.handSize; h++) {
       currentPlayer.hand.push(this.deck.shift());
     }
   },
-  discard: function (currentPlayer) {
+  discard: function (currentPlayer) { // a method that will discard the hand of a player
     for (var h = 0; h < currentPlayer.hand.length; h++) {
       currentPlayer.hand.pop(this.deck.shift());
     }
   },
-  newRound: function () {
-    for (var p = 0; p < this.players.length; p++) {
+  newRound: function () { // a method that will discard any cards on the players hands, then shuffle the deck and deal new hands. Finally, it'll
+    for (var p = 0; p < this.players.length; p++) { // update the point value of each player based on their current hand value
       this.discard(this.players[p]);
     }
     this.deck = _.shuffle(this.deck);
@@ -83,8 +83,8 @@ var game = {
       this.handValue(this.players[p]);
     }
   },
-  endRound: function () {
-    var highestScore = 0;
+  endRound: function () { // a method that will compare each player points total with the highest score, calling the winner in the console unless
+    var highestScore = 0; // there's a tie; in which case a new game will be set between the players tied in first place
     var champions = [];
     for (var p = 0; p < this.players.length; p++) {
       if (this.players[p].points > highestScore) {
@@ -106,4 +106,4 @@ var game = {
   }
 };
 
-console.log(game);
+console.log(game); // without calling the game at some point, the VSC wasn't letting me commit the code
